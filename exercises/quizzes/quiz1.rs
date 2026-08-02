a8679f60fd4e60dc29f0d957b2f06a87af31343e
Yoni <122539345+SytonK@users.noreply.github.com>
2026-08-03 00:53:48 +0300

    q1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,12 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(number_of_apples: u8) -> u8 { 
+    if (number_of_apples > 40) {
+        return number_of_apples;
+    }
+    number_of_apples * 2
+ }
 
 fn main() {
     // You can optionally experiment here.
